07ea34fbefc9d589202ebce728c22a027536e152
FillerDK <fillerdk1234@gmail.com>
2023-09-20 13:29:21 +0200

    "Ruby" + "raw ruby" added

--- src/main/java/net/fillerdk/tutorialmod/TutorialMod.java
+++ src/main/java/net/fillerdk/tutorialmod/TutorialMod.java
@@ -2,6 +2,8 @@ package net.fillerdk.tutorialmod;
 
 import net.fabricmc.api.ModInitializer;
 
+import net.fillerdk.tutorialmod.item.ModItemGroups;
+import net.fillerdk.tutorialmod.item.ModItems;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -11,6 +13,7 @@ public class TutorialMod implements ModInitializer {
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Hello Fabric world!");
+		ModItemGroups.registerItemGroups();
+		ModItems.registerModItems();
 	}
 }
